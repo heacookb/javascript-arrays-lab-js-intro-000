11909797e254f58b1e0aa31d2c14cebe34399750
Brian Heacook <heacookb@protonmail.com>
2018-01-16 18:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+}
+function appendKitten(name)
+{
+  var cats = kittens;
+  cats = [kittens, ...name];
+  return cats;
+}
+function prependKitten(name)
+{
+  var cats = kittens;
+  cats = [...name, kittens];
+  return cats;
+}
