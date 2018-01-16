f4fabf8f2f201afeeafcc89e10a0d0a44a26934d
Brian Heacook <heacookb@protonmail.com>
2018-01-16 19:07:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function destructivelyRemoveFirstKitten()
 function appendKitten(name)
 {
   var cats = kittens;
-  cats = [name, ...kittens]
+  cats = [kittens, ...name];
   return cats;
 }
 function prependKitten(name)
